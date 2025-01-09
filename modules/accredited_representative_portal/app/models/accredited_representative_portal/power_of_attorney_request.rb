1a59b2327bd193631b9829df2db2fe30cf15a77d
Gregory Hedrick <gregory@pixiitech.net>
2025-01-09 10:15:34 -0500

    Changes per review

--- modules/accredited_representative_portal/app/models/accredited_representative_portal/power_of_attorney_request.rb
+++ modules/accredited_representative_portal/app/models/accredited_representative_portal/power_of_attorney_request.rb
@@ -19,9 +19,6 @@ module AccreditedRepresentativePortal
             class_name: 'PowerOfAttorneyRequestResolution',
             inverse_of: :power_of_attorney_request
 
-    has_many :resolutions,
-             class_name: 'PowerOfAttorneyRequestResolution'
-
     belongs_to :power_of_attorney_holder,
                inverse_of: :power_of_attorney_requests,
                polymorphic: true
@@ -36,8 +33,8 @@ module AccreditedRepresentativePortal
       created_at + 60.days
     end
 
-    scope :pending, -> { left_joins(:resolutions).where(resolutions: { resolving_type: nil }) }
-    scope :completed, -> { joins(:resolutions) }
+    scope :pending, -> { left_joins(:resolution).where(resolution: { resolving_type: nil }) }
+    scope :completed, -> { joins(:resolution) }
 
     private
 
